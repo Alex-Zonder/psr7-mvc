81ee2a16d02b09536b4d1b6956443393137d06eb
Aleksandr Pilipenko <f-m@bk.ru>
2021-04-07 23:16:38 +0300

    updated index.php

--- public/index.php
+++ public/index.php
@@ -1,5 +1,35 @@
 <?php
+set_include_path(dirname(__DIR__));
+require 'vendor/autoload.php';
 
-echo phpversion();
+use Zend\Diactoros\ServerRequestFactory;
+use Zend\Diactoros\Response\JsonResponse;
+// use Psr\Http\Message\ServerRequestInterface as Request;
+// use Zend\Diactoros\Response\HtmlResponse;
 
+
+### Get Request ###
+$request = ServerRequestFactory::fromGlobals();
+
+
+
+### Action ###
+$name = $request->getQueryParams()['name'] ?? 'Guest';
+$body = 'Hello: ' . $name;
+// echo $body;
+
+$response = (new JsonResponse($body))
+    ->withHeader('Hello', 'Test')
+    ->withStatus(299);
+
+
+
+### Send Response ###
+if ($response) {
+    header("HTTP/1.0 {$response->getStatusCode()} {$response->getReasonPhrase()}");
+    foreach ($response->getHeaders() as $name => $values) {
+        header($name . ':' . implode(', ', $values));
+    }
+    echo $response->getBody();
+}
 ?>
